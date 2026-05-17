9eb148f21767715542470461709f52d6f8cb9ff9
Cemaleddin Seyhan <cemaleddinseyhan06@gmail.com>
2026-05-17 16:50:51 +0300

    cors config

--- backend/src/main/java/com/school_project/api/config/CorsConfig.java
+++ backend/src/main/java/com/school_project/api/config/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "https://confident-upliftment-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
