e69bcd47d5d17badd150f98c4d6a3e3361d1ecf8
Cemaleddin Seyhan <cemaleddinseyhan06@gmail.com>
2026-05-24 10:05:28 +0300

    dependency added

---
+++ backend/src/main/java/com/school_project/api/config/RedisConfig.java
@@ -0,0 +1,8 @@
+package com.school_project.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+public class RedisConfig {
+}
