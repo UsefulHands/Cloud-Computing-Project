771d8e392ea2803383bc8510270c9e7277503369
Cemaleddin Seyhan <cemaleddinseyhan06@gmail.com>
2026-05-24 12:39:48 +0300

    cacheables added

--- backend/src/main/java/com/school_project/api/config/RedisConfig.java
+++ backend/src/main/java/com/school_project/api/config/RedisConfig.java
@@ -1,12 +1,14 @@
 package com.school_project.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
@@ -20,7 +22,7 @@ public class RedisConfig {
                 .disableCachingNullValues()
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair.fromSerializer(
-                                RedisSerializer.json()
+                                new StringRedisSerializer()
                         )
                 );
         return RedisCacheManager.builder(connectionFactory)
